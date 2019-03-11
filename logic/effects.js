fc465bf60bd56b22a4310a1ba6b3a3a81eea8967
nkenon <37303352+nkenon89@users.noreply.github.com>
2019-03-11 15:24:20 -0400

    added visual changelog via alert for visitors as well as in the console

--- logic/effects.js
+++ logic/effects.js
@@ -1,13 +1,5 @@
 console.log("js file linked");
 
-const changeLog = `
-
-- Resume page failing to load embedded pdf
-
-`;
-
-alert("Hello and thank you for taking the time to view my portfolio! Please keep in mind this project is 'in progress'. The following is the current list of bugs and fixes I am currently working on:" + changeLog);
-
 const headline = document.getElementById('mainHeadline');
 
 mainHeadline.addEventListener('mouseover', function() {
