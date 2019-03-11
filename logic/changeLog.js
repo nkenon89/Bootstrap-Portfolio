983386868ecce991fd15c967f96d50b22ba9e0c7
nkenon <37303352+nkenon89@users.noreply.github.com>
2019-03-11 16:38:31 -0400

    added new project thumnails

--- logic/changeLog.js
+++ logic/changeLog.js
@@ -6,7 +6,7 @@ Bugs & Fixes:
 
 [X] Resume page failing to load embedded pdf
 [ ] Fix scroll cutoff bug on about page  
-[ ] Add js to fix alert popping up every time you visit portfolio page (shoudl appear only upon initial visit)
+[ ] Add js to fix alert popping up every time you visit portfolio page (should appear only upon initial visit)
 
 To-Do's
 
@@ -17,13 +17,13 @@ To-Do's
 [X] Fix link margins
 [ ] Smooth out animation effect on site title/name
 [ ] Wire up contact form
-[ ] Add accurate project thumbnails
+[X] Add accurate project thumbnails
 [ ] Make site mobile responsive
-[ ] "View code" links on portfolio page need to be wired up 
+[X] "View code" links on portfolio page need to be wired up 
 [ ] Add content to about page
 
 `;
 
 console.log(changeLog);
 
-alert("Hello and thank you for taking the time to view my portfolio! Please keep in mind this project is 'in progress'. The following is the current list of bugs and fixes I am currently working on!" + changeLog);
+alert("Hello and thank you for taking the time to view my portfolio! Please keep in mind this project is 'in progress'. The following is the current list of things I am currently working on!" + changeLog);
